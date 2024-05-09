3cfd9a303a53e101bc3acc7bc62547b2f9796d97
Muny1re1 <munyiriandy@gmail.com>
2024-05-09 20:24:37 +0300

    yes

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,28 @@
-// create your App component here
+import React, { useEffect, useState } from "react";
+
+function App() {
+    const [src, setSrc] = useState('');
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+            .then(response => response.json())
+            .then(data => setSrc(data.message))
+            .catch(error => console.error("Error fetching dog image:", error));
+    }, []);
+
+    if (!src) {
+        return (
+            <div>
+                <p>Loading...</p>
+            </div>
+        );
+    }
+
+    return (
+        <div>
+            <img src={src} alt="A Random Dog" />
+        </div>
+    );
+}
+
+export default App;
